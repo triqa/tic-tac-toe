819a38e805eab39b00288bd6607b5d1f32f09ebf
zira <66490318+triqa@users.noreply.github.com>
2026-03-02 00:22:56 +0000

    Add resetScore method to player obj

--- script.js
+++ script.js
@@ -41,20 +41,53 @@ function createPlayer(name, marker) {
     return score;
   }
 
-  return { getPlayerName, getPlayerMarker };
+  // Increase the player's score by 1
+  function increaseScore() {
+    console.log(`${name}'s score has increased by 1`);
+    score++;
+  }
+
+  // Reset player's score to 0
+  function resetScore() {
+    console.log(`${name}'s score has been reset to 0`);
+    score = 0;
+  }
+
+  return {
+    getPlayerName,
+    getPlayerMarker,
+    getScore,
+    increaseScore,
+    resetScore,
+  };
 }
 
 // console.log(gameBoard.newGrid());
 const boris = createPlayer("Boris", "X");
 const tony = createPlayer("Tony", "O");
 
-console.log(boris.getPlayerMarker());
+console.log(boris.getScore());
+
+tony.increaseScore();
+
+console.log(boris.getScore());
+console.log(tony.getScore());
 
 // gameController
 // Single instance: wrap factory func inside IIFE (module pattern)
 // Controls the flow of the game
-// gameController = (() => {})();
+gameController = (() => {
+  return;
+})();
 
 // DisplayController: single instance
 // Display game to the UI
 // Wrap factory inside IIFE (module pattern)
+
+/// RUNNING THE GAME ///
+
+// Create players
+const p1 = createPlayer("p1", "O");
+const p2 = createPlayer("p2", "X");
+// Start (new) game
+gameController.startGame();
